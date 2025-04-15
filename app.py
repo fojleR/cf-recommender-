5f0a60943e84cf0a1b7caaab0301546473a6720b
fojleR <frabbybics10@gmail.com>
2025-04-15 18:28:48 +0600

    updated

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 from tensorflow import keras
 import requests
 from urllib.parse import quote
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -46,4 +47,5 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
